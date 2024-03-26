904c892ea6e9568a1fb738feab85707cf099af90
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2024-03-26 18:03:52 +0000

    feat domain-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const domains = [
+    {
+      pronoun: ["the", "our"]
+    },
+    {
+      adj: ["great", "big"]
+    },
+    {
+      noun: ["jogger", "racoon"]
+    }
+  ];
+
+  let domainHTML = '<ul class="list-group">';
+
+  for (let pronoun of domains[0].pronoun) {
+    for (let adj of domains[1].adj) {
+      for (let noun of domains[2].noun) {
+        domainHTML += `<li class="list-group-item">
+            ${pronoun}${adj}${noun}.com
+          </li>`;
+      }
+    }
+  }
+
+  const dominios = document.querySelector("#domains");
+  dominios.innerHTML = domainHTML;
 };
